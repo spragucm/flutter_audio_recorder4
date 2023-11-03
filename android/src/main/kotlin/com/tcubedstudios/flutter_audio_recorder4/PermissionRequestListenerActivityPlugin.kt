0cb9dddde1e22aef086c6881b0d17ebdab146034
Chris Sprague <spragucm@gmail.com>
2023-11-03 06:37:58 -0700

    WIP - dropping attempt to support Android v1 embedding

--- android/src/main/kotlin/com/tcubedstudios/flutter_audio_recorder4/PermissionRequestListenerActivityPlugin.kt
+++ android/src/main/kotlin/com/tcubedstudios/flutter_audio_recorder4/PermissionRequestListenerActivityPlugin.kt
@@ -1,20 +1,9 @@
 package com.tcubedstudios.flutter_audio_recorder4
 
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
-import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.PluginRegistry
 
 abstract class PermissionRequestListenerActivityPlugin : ActivityAwarePlugin(), PluginRegistry.RequestPermissionsResultListener {
-
-    companion object {
-        fun registerWith(registrar: PluginRegistry.Registrar) {
-            MethodChannel(registrar.messenger(), "flutter_audio_recorder4").apply {
-                val plugin = FlutterAudioRecorder4Plugin()
-                setMethodCallHandler(plugin)
-                registrar.addRequestPermissionsResultListener(plugin)
-            }
-        }
-    }
 
     override fun onAttachedToActivity(binding: ActivityPluginBinding) {
         super.onAttachedToActivity(binding)
