0189b5544902708279ecf4145cd1722a88fb66fc
Chris Sprague <spragucm@gmail.com>
2023-11-04 10:33:29 -0700

    Removed everything for a fresh start.

--- example/android/app/src/main/kotlin/com/example/flutter_audio_recorder3_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_audio_recorder3_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- ios/Classes/FlutterAudioRecorder3Plugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface FlutterAudioRecorder3Plugin : NSObject<FlutterPlugin>
-@end
